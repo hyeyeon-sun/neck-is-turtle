b39a95ea0821f48a2054a49b2fa3e96799f78179
HyeyeonLee <dlgpdus0416@naver.comm>
2023-03-03 15:30:40 +0900

    [이혜연][chatbot 더미 api 호출]

---
+++ server/src/main/java/com/example/neckisturtle/feature/controller/chatbot.java
@@ -0,0 +1,51 @@
+package com.example.neckisturtle.feature.controller;
+
+import com.example.neckisturtle.core.resultMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/chatbot")
+@CrossOrigin
+public class chatbot {
+    @PostMapping(value = "/test", headers = {"Accept=application/json"})
+    public String getMain(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response){
+//
+//        ArrayList<resultMap> outputs = new ArrayList<resultMap>();
+//
+//        resultMap text = new resultMap();
+//        text.put("text", "hello I'm Ryan");
+//        resultMap simpleText = new resultMap();
+//        simpleText.put("simpleText", text);
+//
+//        outputs.add(simpleText);
+//
+//        resultMap template = new resultMap();
+//
+//        template.put("outputs", outputs);
+//
+//        resultMap result = new resultMap();
+//        result.put("vesion", "2.0");
+//        result.put("template", template);
+//
+//
+
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonInString = mapper.writeValueAsString(params);
+            System.out.println(jsonInString);
+            int x = 0;
+        }catch (Exception e){
+
+        }
+
+//        return result;
+
+        return "index";
+    }
+}
