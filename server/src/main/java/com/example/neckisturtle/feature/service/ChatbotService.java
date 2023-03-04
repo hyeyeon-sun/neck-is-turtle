c6cb7164b862b3f9d82734b6f88f693f83d1f9cf
HyeyeonLee <dlgpdus0416@naver.comm>
2023-03-05 00:07:52 +0900

    [이혜연][챗봇 정보 암호화해 보내도록 구현]

--- server/src/main/java/com/example/neckisturtle/feature/service/ChatbotService.java
+++ server/src/main/java/com/example/neckisturtle/feature/service/ChatbotService.java
@@ -1,5 +1,6 @@
 package com.example.neckisturtle.feature.service;
 
+import com.example.neckisturtle.core.AES256;
 import com.example.neckisturtle.core.resultMap;
 import com.example.neckisturtle.feature.domain.Pose;
 import com.example.neckisturtle.feature.domain.User;
@@ -8,10 +9,15 @@ import com.example.neckisturtle.feature.persistance.UserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 @Slf4j
@@ -21,9 +27,36 @@ public class ChatbotService {
 
     private final PoseRepo poseRepo;
 
+
+    public static String alg = "AES/CBC/PKCS5Padding";
+    private final String key = "01234567890123456789012345678901";
+    private final String iv = key.substring(0, 16); // 16byte
+
+
     public ChatbotService(UserRepo userRepo, PoseRepo poseRepo) {
         this.userRepo = userRepo;
         this.poseRepo = poseRepo;
+    }
+
+    public String encrypt(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, "UTF-8");
     }
 
     public resultMap getTodayInfo(String kakaoId) {
@@ -44,6 +77,13 @@ public class ChatbotService {
 
         Pose pose = poseRepo.findByRegDtmAndUserId(new SimpleDateFormat("yyyy-MM-dd").parse(format), user).orElse(defaultPose);
 
+        HashMap<String, Object> infoToFront = new HashMap<>();
+        infoToFront.put("turtleTime", pose.getTurtleTime());
+        infoToFront.put("straightTime", pose.getStraightTime());
+        infoToFront.put("userName", user.getName());
+        infoToFront.put("date", format);
+
+        String encryptedInfo = this.encrypt(infoToFront.toString());
 
 
 //        resultMap text = new resultMap();
@@ -60,11 +100,13 @@ public class ChatbotService {
         template.put("name", kakaoUser.getName());
         template.put("straightTime", pose.getStraightTime());
         template.put("turtieTime", pose.getTurtleTime());
-        template.put("token", "asdfasdf");
+        template.put("encryptedInfo", encryptedInfo);
 
         result.put("template", template);
         return result;
         } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
